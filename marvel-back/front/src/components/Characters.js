851d878250bbad3369a4063c0a9a682f61f8b2f6
sergio <sergiocanalesm1@gmail.com>
2020-03-01 23:38:07 -0500

    funciona sin bootstrap

--- marvel-back/front/src/components/Characters.js
+++ marvel-back/front/src/components/Characters.js
@@ -7,14 +7,17 @@ class Characters extends Component {
     componentDidMount(){
         const url = "http://gateway.marvel.com/v1/public/characters?ts=3&apikey=3019f66588507982e994e44537eb77eb&hash=2a94b0610a4504e68aca2c817a42bac7";
         axios.get(url).then(res => {
-            characters = []
-            for(let i = 0; i<res.data.length;i++){
-                let temp_path = res.data[i].thumbnail.path;
-                let extension = res.data[i].thumbnail.extension;
+            console.log(res.data.data.results);
+            let characters = []
+            //console.log(res.data.length);
+            for(let i = 0; i<res.data.data.results.length;i++){
+                console.log(i);
+                let temp_path = res.data.data.results[i].thumbnail.path;
+                let extension = res.data.data.results[i].thumbnail.extension;
                 let p = `${temp_path}.${extension}`;
                 let character = {
-                    name:res.data[i].name,
-                    description : res.data[i].description,
+                    name:res.data.data.results[i].name,
+                    description : res.data.data.results[i].description,
                     path: p,
                 };
                 characters.push(character);
